a37a5e4678ae3809643929f218598d1b4bc772cf
Justme <746262066@qq.com>
2020-07-30 11:23:29 +0800

    猫眼top100爬虫训练

---
+++ spider.py
@@ -0,0 +1,78 @@
+# -*- coding = utf-8 -*-
+# @Time :2020/7/28 8:47
+# @Author :windows
+# @Site :
+# @File :spider.py
+# @SoftWare :PyCharm
+import json
+import random
+from multiprocessing import Pool
+
+import requests
+from requests.exceptions import RequestException
+import re
+
+# 收集到的常用Header
+my_headers = [
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "
+]
+
+
+def get_one_page(url):
+    try:
+        headers = {
+            'User-Agent': random.choice(my_headers)
+        }
+
+        response = requests.get(url=url, headers=headers)
+        if response.status_code == 200:
+            response.encoding = 'utf-8'
+            return response.text
+    except RequestException:
+        return None
+
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a' +
+                         '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5]+item[6]
+        }
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8')as f:
+        f.write(json.dumps(content, ensure_ascii=False)+'\n')
+        f.close()
+
+def main(offset):
+    url = 'https://maoyan.com/board/4?offset='+str(offset)
+    html = get_one_page(url)
+    # print(html)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    # main()
+    pool.map(main, [i*10 for i in range(10)])
